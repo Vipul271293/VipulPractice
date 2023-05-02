d5217ab5bd963a9495b8a991e8448c2577f63649
Vipul <vipul1427@gmail.com>
2023-05-02 17:18:17 +0530

    Vipul 3 commit

---
+++ src/BasicOops/VipulClass.java
@@ -0,0 +1,13 @@
+package BasicOops;
+
+public class VipulClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("later delete this class created to practice git");
+
+	}
+
+}
